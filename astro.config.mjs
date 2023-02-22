9383b8c1368a2ce42df26909535497e42911d675
Grendel <jcg@grendel.world>
2023-02-21 18:46:55 -0600

    Use deno server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { execFileSync } from "node:child_process";
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://grendel.world",
+  output: "server",
+  adapter: deno(),
+  server: {
+    host: (function () {
+      const ipRoute = execFileSync("/usr/bin/ip", [
+        "--json",
+        "route",
+      ]).toString();
+
+      const ipAddress = JSON.parse(ipRoute)
+        .filter((route) => route.dst === "default")
+        .map((route) => route.prefsrc);
+
+      return ipAddress[0] || "localhost";
+    })(),
+  },
+  markdown: {
+    drafts: true,
+    syntaxHighlighting: "shiki",
+    shikiConfig: {
+      theme: "rose-pine-moon",
+    },
+  },
+});
