f09b3cf723afd58651359dc1e10de664f722f962
Grendel <jcg@grendel.world>
2023-04-19 11:08:08 -0500

    Remove disgusting ip(1) invocation and parsing
    Astro uses Vite to serve it, which accepts the --host option to
    serve it on the local IP address for development and testing
    purposes.  No need for all this grossness to do it manually.
    Simply invoking `npx astro dev --host` suffices, and furthermore it
    also runs on localhost as well as the local IP address, allowing me
    to test things requiring a secure context, like the Clipboard API.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import { execFileSync } from "node:child_process";
 import deno from "@astrojs/deno";
 
 // https://astro.build/config
@@ -7,20 +6,6 @@ export default defineConfig({
   site: "https://grendel.world",
   output: "server",
   adapter: deno(),
-  server: {
-    host: (function () {
-      const ipRoute = execFileSync("/usr/bin/ip", [
-        "--json",
-        "route",
-      ]).toString();
-
-      const ipAddress = JSON.parse(ipRoute)
-        .filter((route) => route.dst === "default")
-        .map((route) => route.prefsrc);
-
-      return ipAddress[0] || "localhost";
-    })(),
-  },
   markdown: {
     drafts: true,
     syntaxHighlighting: "shiki",
